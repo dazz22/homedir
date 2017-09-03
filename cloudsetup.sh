7ec87d1d634f5cab50795d9fa029a09c76da8472
Darren Lanigan <darren.j.lanigan@gmail.com>
2017-09-03 22:58:49 +0000

    fixed issue where, packages were unlocatable

--- cloudsetup.sh
+++ cloudsetup.sh
@@ -34,7 +34,7 @@ function setupgithub()
 
 function setuppythondev()
 {
-
+    sudo apt-get update
     sudo apt-get install -y vim-nox > /dev/null
     sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev \
 	libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \
